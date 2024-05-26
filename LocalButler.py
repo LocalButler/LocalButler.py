31650e59960db6923d98317c31d3c00e472b1670
LocalButler <Blockchainservices2021@gmail.com>
2024-05-26 14:29:01 -0600

    Update LocalButler.py

--- LocalButler.py
+++ LocalButler.py
@@ -47,7 +47,7 @@ def display_meal_delivery_services():
         st.write("- Select pick-up and specify the date and time.")
         st.write("- Let your assigned butler know you've placed an order, and we'll take care of the rest!")
     elif restaurant == "Baltimore Coffee & Tea Company":
-        st.write(f"You selected: [Baltimore Coffee & Tea Company](https://www.baltcoffee.com/)")
+        st.write(f"You selected: [Baltimore Coffee & Tea Company](https://www.baltcoffee.com/sites/default/files/pdf/2023WebMenu_1.pdf)")
         st.write("Instructions for placing your order:")
         st.write("- Review the menu and decide on your order.")
         st.write("- Call Baltimore Coffee & Tea Company to place your order.")
