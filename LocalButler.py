f65312867edf7f3f609ee5ae4b6ddaa82b70e194
LocalButler <Blockchainservices2021@gmail.com>
2024-05-26 12:53:05 -0600

    Update LocalButler.py

--- LocalButler.py
+++ LocalButler.py
@@ -32,25 +32,26 @@ def display_car_wash_services():
 def main():
     st.title("Welcome to Local Butler")
 
-    st.sidebar.title("Local Butler Menu")
-    category = st.sidebar.radio("Select a service category:",
-                                ("Grocery Services", "Laundry Services", "Meal Delivery Services", "Errand Services",
-                                 "Pharmacy Services", "Pet Care Services", "Car Wash Services"))
+    if st.sidebar.button("Start Here"):
+        st.sidebar.title("Local Butler Menu")
+        category = st.sidebar.radio("Select a service category:",
+                                    ("Grocery Services", "Laundry Services", "Meal Delivery Services", "Errand Services",
+                                     "Pharmacy Services", "Pet Care Services", "Car Wash Services"))
 
-    if category == "Grocery Services":
-        display_grocery_services()
-    elif category == "Laundry Services":
-        display_laundry_services()
-    elif category == "Meal Delivery Services":
-        display_meal_delivery_services()
-    elif category == "Errand Services":
-        display_errand_services()
-    elif category == "Pharmacy Services":
-        display_pharmacy_services()
-    elif category == "Pet Care Services":
-        display_pet_care_services()
-    elif category == "Car Wash Services":
-        display_car_wash_services()
+        if category == "Grocery Services":
+            display_grocery_services()
+        elif category == "Laundry Services":
+            display_laundry_services()
+        elif category == "Meal Delivery Services":
+            display_meal_delivery_services()
+        elif category == "Errand Services":
+            display_errand_services()
+        elif category == "Pharmacy Services":
+            display_pharmacy_services()
+        elif category == "Pet Care Services":
+            display_pet_care_services()
+        elif category == "Car Wash Services":
+            display_car_wash_services()
 
 if __name__ == "__main__":
     main()
