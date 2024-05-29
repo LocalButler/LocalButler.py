ebdd8b56f6b37e641ede808ee4a0717831c142f3
LocalButler <Blockchainservices2021@gmail.com>
2024-05-28 19:52:25 -0600

    Update LocalButler.py

--- LocalButler.py
+++ LocalButler.py
@@ -69,9 +69,7 @@ def get_user_by_username(conn, username):
 # Service functions
 def display_grocery_services():
     st.write("Order fresh groceries from your favorite local stores and have them delivered straight to your doorstep.")
-    # Embed the YouTube video
-    video_html = """
-       <iframe src="https://www.youtube.com/embed/aKx6rxVenic?autoplay=1&loop=1&playlist=aKx6rxVenic&controls=0&showinfo=0"
+    <iframe src="https://www.youtube.com/embed/aKx6rxVenic?autoplay=1&loop=1&playlist=aKx6rxVenic&controls=0&showinfo=0"
             style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
             frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
     </iframe>
