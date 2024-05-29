0bc84ac9c47f7d968a5244494355796fec883693
LocalButler <Blockchainservices2021@gmail.com>
2024-05-29 14:48:46 -0600

    Update LocalButler.py

--- LocalButler.py
+++ LocalButler.py
@@ -72,7 +72,7 @@ def display_grocery_services():
     video_html = """
     <div style="position: relative; width: 100%; height: 0; padding-bottom: 56.25%;">
         <iframe src="https://www.youtube.com/embed/aKx6rxVenic?autoplay=1&loop=1&playlist=aKx6rxVenic"
-            style="position: absolute; top: -5; left: 0; width: 100%; height: 100%;"
+            style="position: absolute; top: 0; left: 0; width: 100%; height: 100%;"
             frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen>
         </iframe>
         <div style="position: absolute; top: 0; left: 0; width: 100%; height: 100%; background-color: black; opacity: 0.3;"></div>
