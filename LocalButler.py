2a239df33a35c83a233286d80a728760bc0d5307
LocalButler <Blockchainservices2021@gmail.com>
2024-06-22 17:02:07 -0600

    Update LocalButler.py

--- LocalButler.py
+++ LocalButler.py
@@ -413,12 +413,11 @@ def main():
             st.warning("You are not logged in.")
 
 
-elif choice == "Register":
+    elif choice == "Register":
         st.subheader("Register")
         new_username = st.text_input("Username")
         new_password = st.text_input("Password", type='password')
         confirm_password = st.text_input("Confirm Password", type='password')
-
         if st.button("Register"):
             if not new_username or not new_password or not confirm_password:
                 st.error("Please fill in all fields.")
