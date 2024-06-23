846c7e47f3a3be1153d272cf28205a2c590939dd
LocalButler <Blockchainservices2021@gmail.com>
2024-06-23 09:13:24 -0600

    Update LocalButler.py

--- LocalButler.py
+++ LocalButler.py
@@ -276,10 +276,7 @@ def display_meal_delivery_services():
     st.write(f"ORDER NOW: [{restaurant}]({restaurant_info['url']})")
     st.write("Instructions for placing your order:")
     for instruction in restaurant_info["instructions"]:
-        st.write(f"- {instruction}"),
-         
-         elif "image_url" in store_info:
-        st.image(store_info['image_url'], caption=f"Order Now", use_column_width=True)
+        st.write(f"- {instruction}")
 
 def display_laundry_services():
     st.write("Schedule laundry pickup and delivery services, ensuring your clothes are clean and fresh with minimal effort.")
