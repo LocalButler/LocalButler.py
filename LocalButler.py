42a37fc28da21810c52e431a8e2631d1e3515c96
LocalButler <Blockchainservices2021@gmail.com>
2024-06-23 08:19:13 -0600

    Update LocalButler.py

--- LocalButler.py
+++ LocalButler.py
@@ -99,11 +99,11 @@ GROCERY_STORES = {
     "SafeWay": {
         "url": "https://www.safeway.com/",
         "instructions": [
-            "image_url": "safeway app ads.png" 
             "Place your order directly with Safeway using your own account to accumulate grocery store points and clip your favorite coupons.",
             "Select store pick-up and specify the date and time.",
             "Let Butler Bot know you've placed a pick-up order, and we'll take care of the rest!"
-        ]
+        ],
+        "image_url": "https://raw.githubusercontent.com/LocalButler/streamlit_app.py/main/safeway%20app%20ads.png"
     },
     "Commissary": {
         "url": "https://shop.commissaries.com/",
