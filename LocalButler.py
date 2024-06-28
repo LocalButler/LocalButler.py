1a957b604026721541a116289d3e7cfb127af48f
LocalButler <Blockchainservices2021@gmail.com>
2024-06-27 21:46:22 -0600

    Update LocalButler.py

--- LocalButler.py
+++ LocalButler.py
@@ -218,8 +218,6 @@ def display_how_it_works():
 
 @login_required
 def display_new_order():
-
-
 def send_email(subject, body):
     sender_email = st.secrets["email"]["sender"]
     sender_password = st.secrets["email"]["password"]
